79cb1dba871f2a584212fc82359af8519888f911
TomRoberts97 <37558792+TomRoberts97@users.noreply.github.com>
2021-01-20 21:26:53 +0000

    Began adding Meal and Item classes
    Also added some planning comments to Item.cs

--- ShoppingList/Models/Item.cs
+++ ShoppingList/Models/Item.cs
@@ -2,10 +2,48 @@
 
 namespace ShoppingList.Models
 {
+
+    //design custom ControllerView rows using;
+    // image of an item/meal (each class will need an image field),
+    // Largest Text = Items name
+    //
+
+    // will also need a details page for Meal and Item, to show when clicked on in ControllerView,
+
+    //How can i display a Meal in ControllerView?
+    // Is there an ability to 'Expand' like the arrows in the Solution Explorer
+    // A Meal can be 'Expanded' into its Items
+    // Can this be done in the ControllerView (Prefered option) (need working delete on Items, On LongClick) or will I need 
+
+    //ShoppingList Main ControllerView;
+    // click - Show EditItem/EditMeal Page
+    // LongClick - Delete Item
+    // Details for items another way, Could have details page in flyout thing 
+
+
+    // look into search feature on a controller view, or a way a user can search for a Meal or Item by Name, ItemType ect
     public class Item
     {
         public string Id { get; set; }
-        public string Text { get; set; }
+        public string Text { get; set; } // this can be seen as Name (change after seeing functionality of variable)
         public string Description { get; set; }
+
+
+        public int MyProperty { get; set; }
+        
+        // ItemType (Fruit, Veg , ect)(go over shop categories)(Use a drop down list for user input)
+
+        // measurement (ml,grams ect)(drop down or way to 
+
+        // Quantity (number of item
+
+
+
+
+        // ideas
+        // Price Estimate? (needs shop data , or some sort of API )
+        //
+
+
     }
 }
---
+++ ShoppingList/Models/Meal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingList.Models
+{
+    class Meal
+    {
+
+        // does a Meal need a unique reference?
+        public int Id{ get; set; } // Unique key (how to generate? size of Meal array + 1?)
+        public string Name { get; set; } // name of the meal 
+
+        public List<Item> IngredientsList { get; set; } // List<Item> of needed ingredients to make Meal
+
+        public int PortionAmount { get; set; } //amount of people meal can serve (as entered by the user , relative to the amounts of each ingredient)
+
+
+
+        public string RecipeLink { get; set; }// link to meal recipe (if it has one)
+
+        public int TasteRating { get; set; }// 1-10 rating of the meal 
+
+
+        // could add 
+        // time to cook (hh:mm) (would be in the recipe , doesnt matter if not recipe linked)
+        // 
+    }
+}
