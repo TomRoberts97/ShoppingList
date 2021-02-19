e96dfadc159e4d3983312eb4ad58bc375491fc9f
TomRoberts97 <37558792+TomRoberts97@users.noreply.github.com>
2021-02-19 18:50:37 +0000

    Working ImageButton and Search Bar features

--- ShoppingList/obj/Debug/netstandard2.0/Views/NewItemPage.xaml.g.cs
+++ ShoppingList/obj/Debug/netstandard2.0/Views/NewItemPage.xaml.g.cs
@@ -17,15 +17,11 @@ namespace ShoppingList.Views {
     public partial class NewItemPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
-        private global::Xamarin.Forms.SearchBar searchBar;
-        
-        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private global::Xamarin.Forms.ListView searchResults;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(NewItemPage));
-            searchBar = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.SearchBar>(this, "searchBar");
             searchResults = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.ListView>(this, "searchResults");
         }
     }
